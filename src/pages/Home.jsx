e927bcb4b360e57228f6c5262d7840a7ae646c7d
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-06 23:35:24 +0530

    added fetch from tvmaze

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
 // import { Link } from "react-router-dom";
 
+import { useState } from "react";
 const Home = () => {
-    return <div>
-        {/* <Link to="/Starred"> Go to Starred</Link> */}
-        <h1>Home Page</h1>
-    </div>;
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const onSearch = async (e) => {
+        e.preventDefault();
+        // console.log("searchTerm", searchTerm);
+
+        const apiResponse = await fetch(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+        const body = apiResponse.then(r => r.json())
+        console.log("body", body);
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSearch}>
+                <input type="text" placeholder="Search" value={searchTerm} onChange={handleSearch} />
+                <button type="submit" > search  </button>
+            </form>
+        </div>
+    );
 }
     
 export default Home;
