ae6add0b1a9d1a57d6fcb79645e69899bd7b4ec1
love-sonkar <loveboss806@gmail.com>
2023-10-05 14:15:50 +0530

    added pikachu running loader effect

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Gif from "./pikachu.gif"
+
+const Loader = () => {
+  return (
+    <>
+      <img src={Gif} alt="" height={150}  />
+    </>
+  );
+};
+
+export default Loader;
